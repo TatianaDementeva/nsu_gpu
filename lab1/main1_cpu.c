0dd5d598cd9f396f8dc55a6edd23dab39ecd8156
Tatiana Dementeva <tdementeva@rit-it.com>
2022-11-01 13:49:14 +0700

    Start lab1

---
+++ lab1/main1_cpu.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+ 
+#define PI 3.14159265
+#define uint unsigned int 
+
+
+int main(int argc, char* argv[])
+{
+  const uint N = pow(10, 6);
+
+  double array_sin[N];
+  double sum_sin = 0.0;
+
+
+  for(int i = 0; i < N; i++)
+    array_sin[i] = sin(2*PI*i/N);
+    
+  for(int i = 0; i < N; i++) 
+    sum_sin += array_sin[i];
+
+  printf("Сумма sin = %2.5f\n", sum_sin);
+
+  return 0;
+}
